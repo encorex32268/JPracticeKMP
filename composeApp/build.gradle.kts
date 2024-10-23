b42b3d2e84a9de975029937cf76fdb3a7e7a67a1
LiHan <encorex32268@gmail.com>
2024-10-23 12:12:25 +0900

    project setting done.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,12 +1,14 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.konfigplugin)
 }
 
 kotlin {
@@ -33,16 +35,30 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.koin.android)
+            implementation(libs.core.splashscreen)
+            implementation(libs.google.playServices.ads)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            implementation(libs.library.base)
+            implementation(libs.library.sync)
+            implementation(libs.kotlinx.coroutines.core)
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+            implementation(libs.androidx.navigation)
+            implementation(libs.lifecycle.viewmodel.compose)
+            implementation(libs.keysettings.kmp)
         }
     }
 }
@@ -78,3 +94,12 @@ dependencies {
     debugImplementation(compose.uiTooling)
 }
 
+
+buildkonfig {
+    packageName = "chen.example.lee.jppractice"
+
+    defaultConfigs {
+
+    }
+
+}
